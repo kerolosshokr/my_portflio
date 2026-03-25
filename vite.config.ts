92882ae2466501cffd8d174bc7b8c5adb0e1522a
User <user@example.com>
2026-03-26 00:53:40 +0200

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Protflio2/',
+  base: '/Portflio2/',
   server: {
     host: "::",
     port: 8080,
